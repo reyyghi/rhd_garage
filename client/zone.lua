00a22ef9951b3d7139e131777e695a47683d0491
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-07-20 04:09:54 +0700

    Update zone.lua

--- client/zone.lua
+++ client/zone.lua
@@ -10,26 +10,26 @@ CreateThread(function ()
                 Utils.createGarageZone({
                     coords = v.location[i].xyz,
                     debug = false,
-                    inside = function ()
-                        if not Zone.drawtext then
-                            
-                            if not v.impound then
-                                if v.gang then if not Utils.GangCheck({garage = k, gang = v.gang}) then Zone.drawtext = not Zone.drawtext return end end
-                                if v.job then if not Utils.JobCheck({garage = k, job = v.job}) then Zone.drawtext = not Zone.drawtext return end end
-                            end
+                    enter = function ()
+                        if Zone.drawtext then return end
+                        Utils.drawtext('show', k:upper(), 'warehouse')
+                        Zone.drawtext = not Zone.drawtext
 
-                            Utils.createGarageRadial({
-                                gType = v.impound and 'impound' or garageTipe,
-                                vType = v.type or 'car',
-                                garage = k,
-                                coords = v.location[i]
-                            })
-
-                            Utils.drawtext('show', k:upper(), 'warehouse')
-                            Zone.drawtext = not Zone.drawtext
+                        if not v.impound then
+                            if v.gang then if not Utils.GangCheck({garage = k, gang = v.gang}) then Zone.drawtext = not Zone.drawtext return end end
+                            if v.job then if not Utils.JobCheck({garage = k, job = v.job}) then Zone.drawtext = not Zone.drawtext return end end
                         end
+
+                        Utils.createGarageRadial({
+                            gType = v.impound and 'impound' or garageTipe,
+                            vType = v.type or 'car',
+                            garage = k,
+                            coords = v.location[i]
+                        })
                     end,
                     exit = function ()
+                        Utils.drawtext('hide')
+                        Zone.drawtext = false
                         Utils.removeRadial(v.impound and 'impound' or garageTipe)
                     end
                 })
@@ -38,23 +38,24 @@ CreateThread(function ()
             Utils.createGarageZone({
                 coords = v.location.xyz,
                 debug = false,
-                inside = function ()
-                    if not Zone.drawtext then
-                        if v.gang then if not Utils.GangCheck({garage = k, gang = v.gang}) then Zone.drawtext = not Zone.drawtext return end end
-                        if v.job then if not Utils.JobCheck({garage = k, job = v.job}) then Zone.drawtext = not Zone.drawtext return end end
+                enter = function ()
+                    if Zone.drawtext then return end
+                    Utils.drawtext('show', k:upper(), 'warehouse')
+                    Zone.drawtext = not Zone.drawtext
 
-                        Utils.createGarageRadial({
-                            gType = v.impound and 'impound' or garageTipe,
-                            vType = v.type or 'car',
-                            garage = k,
-                            coords = v.location
-                        })
+                    if v.gang then if not Utils.GangCheck({garage = k, gang = v.gang}) then Zone.drawtext = not Zone.drawtext return end end
+                    if v.job then if not Utils.JobCheck({garage = k, job = v.job}) then Zone.drawtext = not Zone.drawtext return end end
 
-                        Utils.drawtext('show', k:upper(), 'warehouse')
-                        Zone.drawtext = not Zone.drawtext
-                    end
+                    Utils.createGarageRadial({
+                        gType = v.impound and 'impound' or garageTipe,
+                        vType = v.type or 'car',
+                        garage = k,
+                        coords = v.location
+                    })
                 end,
                 exit = function ()
+                    Utils.drawtext('hide')
+                    Zone.drawtext = false
                     Utils.removeRadial(v.impound and 'impound' or garageTipe)
                 end
             })
