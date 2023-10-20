b22a57dca12a14bf8bfef11897773f7e295d2bc5
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-10-20 09:40:39 +0700

    added new command for admin

--- server/command.lua
+++ server/command.lua
@@ -1,1 +1,12 @@
-[comming soon]
+lib.addCommand(locale("rhd_garage:command.admin.CreateGarage"), {
+    help = locale("rhd_garage:command.admin.CreateGarageHelp"),
+    restricted = 'group.admin'
+}, function(source, args, raw)
+    TriggerClientEvent("rhd_garage:client:createGarage", source)
+end)
+lib.addCommand(locale("rhd_garage:command.admin.listgarage"), {
+    help = locale("rhd_garage:command.admin.listgarageHelp"),
+    restricted = 'group.admin'
+}, function(source, args, raw)
+    TriggerClientEvent("rhd_garage:client:createGarage", source)
+end)
