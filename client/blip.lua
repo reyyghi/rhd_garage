39fc4aa3ed386cd040cc4baa3fda48122be2896d
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-10-20 09:33:51 +0700

    update blip function

--- client/blip.lua
+++ client/blip.lua
@@ -1,17 +1,18 @@
-CreateThread(function()
-    for k, v in pairs(Config.Garages) do
+local blip = {}
+
+function blip.refresh ( data )
+    data = data or GarageZone
+    for k, v in pairs(data) do
         if v.blip ~= nil then
             local location = nil
-            if type(v.location) == 'table' then
-                for i=1, #v.location do
-                    location = v.location[i]
+            local points = v.zones.points
+            if type(points) == 'table' then
+                for i=1, #points do
+                    location = points[i]
                 end
-            elseif type(v.location) == 'vector4' then
-                location = v.location
             end
 
             local GarageBlip = AddBlipForCoord(location.x, location.y, location.z)
-                    
             SetBlipSprite(GarageBlip, v.blip['type'])
             SetBlipScale(GarageBlip, 0.9)
             SetBlipColour(GarageBlip, v.blip['color'])
@@ -22,6 +23,7 @@ CreateThread(function()
             AddTextComponentString(k)
             EndTextCommandSetBlipName(GarageBlip)
         end
-        
     end
-end)
+end
+
+return blip
