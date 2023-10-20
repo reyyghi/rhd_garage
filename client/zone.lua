f317d1fa50532af6cfdb452179f76c4cb93b28ef
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-10-20 18:33:41 +0700

    update zone.client

--- client/zone.lua
+++ client/zone.lua
@@ -1,8 +1,9 @@
-local Zones = require "modules.zone"
 local Utils = require "modules.utils"
 local blips = require "client.blip"
 
-local refreshZone = function ( data )
+local zone = {}
+
+function zone.refresh ( data )
     data = data or GarageZone
     blips.refresh(data)
     for k, v in pairs(data) do
@@ -71,97 +72,15 @@ local refreshZone = function ( data )
     end
 end
 
-local saveData = function ( data )
+function zone.save ( data )
     TriggerServerEvent("rhd_garage:server:saveGarageZone", data)
 end
 
 CreateThread(function ()
     if LocalPlayer.state.isLoggedIn then
-        refreshZone()
+        zone.refresh()
         blips.refresh()
     end
 end)
 
-AddEventHandler('QBCore:Client:OnPlayerLoaded', refreshZone)
-RegisterNetEvent("rhd_garage:client:refreshZone", refreshZone)
-RegisterNetEvent("rhd_garage:client:createGarage", function ()
-    Zones.startCreator({
-        type = "poly",
-        onCreated = function (zones)
-            local input = lib.inputDialog('RHD GARAGE (Creator)', {
-                { type = 'input', label = 'Garage Label', placeholder = 'Alta Garage', required = true },
-                { type = 'select', label = 'Vehicle Type', options = {
-                    {value = "car", label = "Car"},
-                    {value = "boat", label = "Boats"},
-                    {value = "helicopter", label = "Helicopter"},
-                    {value = "planes", label = "Planes"}
-                }, required = true},
-                { type = 'checkbox', label = "Blip"},
-                { type = 'checkbox', label = "Impound"},
-                { type = 'checkbox', label = "Shared"},
-            })
-            
-            if input then
-                local newData = {
-                    label = input[1],
-                    type = input[2],
-                    blip = { type = 357, color = 3 },
-                    zones = zones,
-                    job = {},
-                    impound = not input[5] and input[4] or false,
-                    shared = input[5]
-                }
-
-                if input[3] then
-                    local blipinput = lib.inputDialog('BLIP', {
-                        { type = 'number', label = 'Blip Type', placeholder = '357'},
-                        { type = 'number', label = 'Blip Color', placeholder = '3'},
-                    })
-
-                    if blipinput then
-                        newData.blip.type = blipinput[1] or 357
-                        newData.blip.color = blipinput[2] or 3
-                    end
-
-                    GarageZone[newData.label] = {
-                        type = newData.type,
-                        blip = newData.blip,
-                        zones = newData.zones,
-                        job = newData.job,
-                        impound = newData.impound,
-                        shared = newData.shared
-                    }
-                    
-                    saveData( GarageZone )
-                    return
-                end
-
-                GarageZone[newData.label] = {
-                    type = newData.type,
-                    blip = newData.blip,
-                    zones = newData.zones,
-                    job = newData.job,
-                    impound = newData.impound,
-                    shared = newData.shared
-                }
-                
-                saveData( GarageZone )
-            end
-            
-        end
-    })
-end)
-
-RegisterNetEvent("rhd_garage:client:listgarage", function ()
-    local context = {
-        id = 'rhd:list_garage',
-        title = locale("rhd_garage:context.admin.listgarage_title"),
-        options = {}
-    }
-    for k, v in pairs(GarageZone) do
-        context.options[#context.options+1] = {
-            title = k:upper(),
-            icon = ""
-        }   
-    end
-end)
+return zone
