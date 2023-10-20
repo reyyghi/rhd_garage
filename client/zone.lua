cdcc153605fb9091eba5014fe02abee1bfbe4e59
reyyghi <reyhafizh07@gmail.com>
2023-10-20 23:15:27 +0700

    update zone.main

--- client/zone.lua
+++ client/zone.lua
@@ -1,16 +1,25 @@
 local Utils = require "modules.utils"
 local blips = require "client.blip"
 
+local CreatedZone = {}
 local zone = {}
 
 function zone.refresh ( data )
     data = data or GarageZone
     blips.refresh(data)
+
     for k, v in pairs(data) do
-        lib.zones.poly({
+        if CreatedZone[k] then
+            CreatedZone[k]:remove()
+        end
+    end
+
+    for k, v in pairs(data) do
+        CreatedZone[k] = lib.zones.poly({
             points = v.zones.points,
             thickness = v.zones.thickness,
             onEnter = function ()
+                print(json.encode(v.job))
                 if not v.impound then
                     if v.gang then if not Utils.GangCheck({garage = k, gang = v.gang}) then return end end
                     if v.job then if not Utils.JobCheck({garage = k, job = v.job}) then return end end
