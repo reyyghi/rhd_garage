6e80fb90ded42d08e3b7197820997cd8342386eb
reyyghi <reyhafizh07@gmail.com>
2023-10-24 01:22:35 +0700

    update server.main

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,166 @@
-lib.callback.register('rhd_garage:cb:removeMoney', function(src, type, amount)
+--- callback
+lib.callback.register('rhd_garage:cb_server:removeMoney', function(src, type, amount)
     return Framework.server.removeMoney(src, type, amount)
 end)
 
+lib.callback.register('rhd_garage:cb_server:getvehowner', function (src, plate, shared)
+    local vehicledata = {}
+    local isQB = Framework.qb()
+    local isOwner = true
+    if isQB then
+        vehicledata.cid = Framework.server.GetPlayer(src).PlayerData.citizenid
+        vehicledata.dbtable = "SELECT 1 FROM `player_vehicles` WHERE `citizenid` = ? and plate = ?"
+        vehicledata.dbvalue = {vehicledata.cid, plate}
+
+        if shared then
+            vehicledata.dbtable = "SELECT 1 FROM `player_vehicles` WHERE plate = ? OR fakeplate = ?"
+            vehicledata.dbvalue = { plate }
+        end
+    else
+        vehicledata.cid = Framework.server.GetPlayer(src).identifier
+        vehicledata.dbtable = "SELECT `vehicle` FROM `owned_vehicles` WHERE `owner` = ? and plate = ?"
+        vehicledata.dbvalue = { vehicledata.cid, plate }
+
+        if shared then
+            vehicledata.dbtable = "'SELECT vehicle FROM owned_vehicles WHERE plate = ?"
+            vehicledata.dbvalue = { plate }
+        end
+    end
+
+    local result = MySQL.single.await(vehicledata.dbtable, vehicledata.dbvalue)
+
+    if not result then
+        isOwner = not isOwner
+    end
+    
+     return isOwner
+end)
+
+
+lib.callback.register('rhd_garage:cb_server:getVehicleList', function(src, garage, impound, shared)
+    local impound_garage = impound
+    local shared_garage = shared
+
+    local garageData = {}
+    local isQB = Framework.qb()
+
+    garageData.vehicle = {}
+
+    if isQB then
+        garageData.cid = Framework.server.GetPlayer(src).PlayerData.citizenid
+        garageData.dbtable = "SELECT vehicle, mods, state, plate, fakeplate FROM player_vehicles WHERE garage = ? and citizenid = ?"
+        garageData.dbvalue = {garage, garageData.cid}
+
+        if impound_garage then
+            if shared_garage then return false end
+            garageData.dbtable = "SELECT vehicle, mods, state, plate, fakeplate FROM player_vehicles WHERE state = ? and citizenid = ?"
+            garageData.dbvalue = {0, garageData.cid}
+        end
+
+        if shared_garage then
+            garageData.dbtable = "SELECT player_vehicles.vehicle, player_vehicles.mods, player_vehicles.state, player_vehicles.plate, player_vehicles.fakeplate, players.charinfo FROM player_vehicles LEFT JOIN players ON players.citizenid = player_vehicles.citizenid WHERE player_vehicles.garage = ?"
+            garageData.dbvalue = {garage}
+        end
+    else
+        garageData.cid = Framework.server.GetPlayer(src).identifier
+        garageData.dbtable = "SELECT vehicle, plate, stored FROM owned_vehicles WHERE garage = ? and owner = ?"
+        garageData.dbvalue = {garage, garageData.cid}
+
+        if impound_garage then
+            if shared_garage then return false end
+            garageData.dbtable = "SELECT vehicle, plate, stored FROM owned_vehicles WHERE stored = ? and owner = ?"
+            garageData.dbvalue = {0, garageData.cid}
+        end
+
+        if shared_garage then
+            garageData.dbtable = "SELECT owned_vehicles.vehicle, owned_vehicles.plate, owned_vehicles.stored, users.firstname, users.lastname FROM owned_vehicles LEFT JOIN users ON users.identifier = owned_vehicles.owner WHERE owned_vehicles.garage = ?"
+            garageData.dbvalue = {garage}
+        end
+    end
+
+    local result = MySQL.query.await(garageData.dbtable, garageData.dbvalue)
+
+    if result and next(result) then
+        if isQB then
+            for k, v in pairs(result) do
+                local charinfo = json.decode(v.charinfo)
+                local vehicles = json.decode(v.mods)
+                local state = v.state
+                local model = v.vehicle
+                local plate = v.plate
+                local fakeplate = v.fakeplate
+                local ownername = charinfo and ("%s %s"):format(charinfo.firstname, charinfo.lastname)
+                
+                garageData.vehicle[#garageData.vehicle+1] = {
+                    vehicle = vehicles,
+                    state = state,
+                    model = model,
+                    plate = plate,
+                    fakeplate = fakeplate,
+                    owner = ownername
+                }
+            end
+        else
+            for k,v in pairs(result) do
+                local vehicles = json.decode(v.vehicle)
+                local state = v.stored
+                local model = vehicles.model
+                local plate = v.plate
+                local ownername = ("%s %s"):format(v.firstname, v.lastname)
+
+                garageData.vehicle[#garageData.vehicle+1] = {
+                    vehicle = vehicles,
+                    state = state,
+                    model = model,
+                    plate = plate,
+                    owner = ownername
+                }
+            end
+        end
+    end
+    return garageData.vehicle
+end)
+
+lib.callback.register('rhd_garage:cb_server:getvehicledatabyplate', function (src, plate)
+    local db = {}
+    local ownerName = "Unkown"
+
+    if Framework.qb() then
+        db.s = "SELECT player_vehicles.vehicle, player_vehicles.mods, player_vehicles.balance, player_vehicles.citizenid, players.charinfo FROM player_vehicles LEFT JOIN players ON players.citizenid = player_vehicles.citizenid WHERE plate = ? OR fakeplate = ?"
+    elseif Framework.esx() then
+        db.s = "SELECT owned_vehicles.vehicle, owned_vehicles.plate, owned_vehicles.owner, users.firstname, users.lastname FROM owned_vehicles LEFT JOIN users ON users.identifier = owned_vehicles.owner WHERE plate = ?"
+    end
+
+    local data = MySQL.single.await(db.s, { plate })
+    if not data then return {} end
+
+    db.data = {}
+
+    if Framework.qb() then
+        local mods = json.decode(data.mods)
+        local charinfo = json.decode(data.charinfo)
+        ownerName = ("%s %s"):format(charinfo.firstname, charinfo.lastname)
+        db.data = {
+            owner = ownerName,
+            vehicle = data.vehicle,
+            props = mods,
+            balance = data.balance
+        }
+    elseif Framework.esx() then
+        ownerName = ("%s %s"):format(data.firstname, data.lastname)
+        local mods = json.decode(data.vehicle)
+        db.data = {
+            owner = ownerName,
+            vehicle = mods.model,
+            props = mods,
+            balance = 0
+        }
+    end
+
+    return db.data
+end)
+
+--- events
 RegisterNetEvent("rhd_garage:server:updateState", function ( data )
     local prop = data.prop
     local state = data.state
@@ -91,6 +250,7 @@ RegisterNetEvent("rhd_garage:server:saveCustomVehicleName", function (fileData)
     SaveResourceFile(GetCurrentResourceName(), 'data/customname.lua', serializedData, -1)
 end)
 
+--- exports
 exports("Garage", function ()
     return GarageZone
 end)
