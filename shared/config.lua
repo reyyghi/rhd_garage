d45c2f3ca8e4369a7d80792e281bb72a6b168ec2
reyyghi <139941203+dellaaaaaa@users.noreply.github.com>
2024-01-20 01:23:29 +0700

    added new configuration

--- shared/config.lua
+++ shared/config.lua
@@ -7,7 +7,9 @@ Config.RadialMenu = 'qb' --- ox / qb / rhd
 Config.FuelScript = 'LegacyFuel' --- rhd-fuel / ox_fuel / LegacyFuel / ps-fuel / cdn-fuel
 Config.changeNamePrice = 15000 --- price for changing the name of the vehicle in the garage
 Config.UsePoliceImpound = true --- change it to false if you don't want to use the police impound system from rhd
+Config.SpawnInVehicle = true --- change this to true if you want the player to immediately enter the vehicle when the vehicle is taken out of the garage
 
+Config.IconAnimation = "fade"
 Config.Icons = {
     car = "car",
     boat = "sailboat",
