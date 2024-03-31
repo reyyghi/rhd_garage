e3a5e3b8d9251be192d675dbe43eb82770554e34
reyyghi <reyhafizh07@gmail.com>
2024-04-01 02:11:09 +0700

    update manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,26 +3,26 @@ game 'gta5'
 author 'Reyghita Hafizh Firmanda'
 version '1.3.4'
 
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua',
+    'bridge/houses/*.lua',
+    'bridge/framework/*.lua'
+}
+
 client_scripts {
-    'bridge/framework/**/client.lua',
     'client/*.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'bridge/framework/**/server.lua',
     'server/main.lua',
     'server/police_impound.lua',
     'server/dbformat.lua',
     'server/storage.lua',
+    'server/vehicle.lua',
     'server/version.lua',
     'server/command.lua',
-}
-
-shared_scripts {
-    '@ox_lib/init.lua',
-    'bridge/shared.lua',
-    'shared/config.lua'
 }
 
 files {
