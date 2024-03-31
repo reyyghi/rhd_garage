50836fba00df6731396edfabf5002c17e67adc50
reyyghi <reyhafizh07@gmail.com>
2024-04-01 02:07:00 +0700

    remove old bridge

--- bridge/shared.lua
+++
@@ -1,9 +0,0 @@
-Framework = {}
-
-function Framework.esx()
-    return GetResourceState("es_extended") ~= "missing"
-end
-
-function Framework.qb()
-    return GetResourceState("qb-core") ~= "missing"
-end
