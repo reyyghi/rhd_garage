f05702ec46d152e84b213fe1b5c23e7e7097c73e
reyyghi <reyhafizh07@gmail.com>
2024-04-02 02:24:58 +0700

    client/vehicle: added new functions

--- client/vehicle.lua
+++ client/vehicle.lua
@@ -1,11 +1,41 @@
-local utils = require "modules.utils"
 vehFunc = {}
+
+local utils = require "modules.utils"
 
 --- Get Vehicle Info By Plate
 ---@param plate string
 function vehFunc.gvibp(plate)
     local vehInfo = lib.callback.await('rhd_garage:cb_server:getVehicleInfoByPlate', false, plate)
     return vehInfo and next(vehInfo) and vehInfo or false 
+end
+
+--- Get Outside Vehicle By Plate
+---@param plate any
+---@return integer | boolean
+function vehFunc.govbp(plate)
+    local Vehicles = GetGamePool("CVehicle")
+    for i=1, #Vehicles do
+        local entity = Vehicles[i]
+        if DoesEntityExist(entity) then
+            local vP = utils.getPlate(entity)
+            if vP == plate then
+                return entity
+            end
+        end
+    end
+    return false
+end
+
+--- Get Vehicle Properties
+---@param vehicle string | integer
+function vehFunc.gvp(vehicle)
+    return lib.getVehicleProperties(vehicle)
+end
+
+---@param vehicle string | integer
+---@param props table
+function vehFunc.svp(vehicle, props)
+    return lib.setVehicleProperties(vehicle, props)
 end
 
 local function getvehdataForPhone ( )
@@ -42,7 +72,7 @@ lib.callback.register('rhd_garage:cb_client:vehicleSpawned', function(netId, pro
     end
 
     if props then
-        lib.setVehicleProperties(veh, props)
+        vehFunc.svp(veh, props)
     end
 
     if deformation then
