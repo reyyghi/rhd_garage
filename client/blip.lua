5d5976a5225bcb4693ffb0a2a7556499921605f9
reyyghi <reyhafizh07@gmail.com>
2024-07-07 14:50:18 +0700

    updated the blips creation function

--- client/blip.lua
+++ client/blip.lua
@@ -1,9 +1,10 @@
-local blip = {}
-local GarageBlip = {}
+gb = {}
+
+local GarageBlip = {} ---@type table<string, integer>
 
 --- Refresh Blip
----@param data table <string, integer>
-function blip.refresh ( data )
+---@param data table <string, GarageData>
+function gb.refresh ( data )
     data = data or GarageZone
     if not data or type(data) ~= "table" then return end
     if GarageBlip and next(GarageBlip) then
@@ -17,24 +18,24 @@ function blip.refresh ( data )
     GarageBlip = {}
     for k, v in pairs(data) do
         if v.blip then
-            local location = nil
+            local location ---@as vector3
             local points = v.zones.points
+           
             if type(points) == 'table' then
                 for i=1, #points do
                     location = points[i]
                 end
             end
+
             GarageBlip[k] = AddBlipForCoord(location.x, location.y, location.z)
-            SetBlipSprite(GarageBlip[k], v.blip['type'])
+            SetBlipSprite(GarageBlip[k], v.blip.type)
             SetBlipScale(GarageBlip[k], 0.9)
-            SetBlipColour(GarageBlip[k], v.blip['color'])
+            SetBlipColour(GarageBlip[k], v.blip.color)
             SetBlipDisplay(GarageBlip[k], 4)
             SetBlipAsShortRange(GarageBlip[k], true)
             BeginTextCommandSetBlipName("STRING")
-            AddTextComponentString(k)
+            AddTextComponentString(v.blip.label or k)
             EndTextCommandSetBlipName(GarageBlip[k])
         end
     end
 end
-
-return blip
