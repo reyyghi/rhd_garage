ec34fb0f8e9596309a1756b719d429a22396390b
reyyghi <reyhafizh07@gmail.com>
2024-09-11 14:38:32 +0700

    remove old file

--- server/command.lua
+++
@@ -1,6 +0,0 @@
-lib.addCommand(locale("command.admin.garagelist"), {
-    help = locale("command.admin.garagelistHelp"),
-    restricted = 'group.admin'
-}, function(source, args, raw)
-    TriggerClientEvent("rhd_garage:client:garagelist", source)
-end)
