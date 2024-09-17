1df862fbbb40c893e89f00d453fd31e463364115
reyyghi <reyhafizh07@gmail.com>
2024-09-17 17:40:48 +0700

    Create garage (qbox).sql

---
+++ sql/garage (qbox).sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `user_vehicles` (
+  `identifier` varchar(46) DEFAULT NULL,
+  `owner_name` longtext DEFAULT NULL,
+  `label` longtext DEFAULT NULL,
+  `model` varchar(50) NOT NULL DEFAULT '',
+  `plate` longtext NOT NULL,
+  `fakeplate` longtext DEFAULT NULL,
+  `garage` longtext DEFAULT 'Motel Parking',
+  `state` int(11) DEFAULT 0,
+  `fuel` float DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `properties` longtext NOT NULL,
+  `deformation` longtext DEFAULT NULL,
+  `logs` longtext NULL DEFAULT '[]',
+  UNIQUE KEY `Index 2` (`plate`(100)),
+  KEY `Index 1` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
