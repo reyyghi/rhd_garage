d1fe47aa9cebf453c615c8b09638c6a147ab988f
Reyghita Hafizh <reyhafizh07@gmail.com>
2025-05-05 11:16:01 +0700

    removed 😆

--- modules/bridge/init.lua
+++
@@ -1,8 +0,0 @@
-local isStarted = function (resource)
-    return GetResourceState(resource) == 'started'
-end
-
-local fileType = IsDuplicityVersion() and 'server' or 'client'
-local framework = isStarted('es_extended') and 'esx' or 'qb'
-
-lib.load(('modules.bridge.%s.%s'):format(framework, fileType))
